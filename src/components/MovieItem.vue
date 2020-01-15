758ecd8d3be7145fdf205af5b5516c1556b18be0
Taptic <taptic@Taptics-MacBook-Pro.local>
2020-01-15 18:58:31 +0330

    video 16-2 end

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -9,7 +9,9 @@
                  <span class="movie-rating">{{ movie.Rated }}</span>
              </div>
              <div class="movie-sessions">
-
+                <div v-for="session in sessions" class="session-time-wrapper">
+                   <div class="session-time">{{ formatSessionTime(session.time) }}</div>
+                </div>
              </div>
          </div>
     </div>
@@ -17,7 +19,12 @@
 
 <script>
 export default {
-    props:['movie','sessions']
+    props:['movie','sessions'],
+    methods:{
+        formatSessionTime(raw){
+            return this.$moment(raw).format('h:mm A')
+        }
+    }
 }
 </script>
 <style lang="">
