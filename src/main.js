c72e4c0f84672efa0757fd067fbe50aaaa546316
mary khodaparast <maryam.kh1374@gmail.com>
2020-01-16 12:35:28 +0330

    video 18-5

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import './style.scss'
-import Overview  from './components/Overview.vue'
+//import Overview  from './components/Overview.vue'
 
 import VueResourse from 'vue-resource'
 Vue.use(VueResourse)
@@ -26,9 +26,6 @@ new Vue({
 		moment,
         day:moment(),
 		bus
-	},
-	components:{
-      Overview
 	},
 	created(){
       this.$http.get('/api').then(response => {
