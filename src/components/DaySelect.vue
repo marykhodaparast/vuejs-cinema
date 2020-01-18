fd71d582d10a4fbd82a178232ff6e58ef912d8b4
mary khodaparast <maryam.kh1374@gmail.com>
2020-01-18 09:00:10 +0330

    video 19-3

--- src/components/DaySelect.vue
+++ src/components/DaySelect.vue
@@ -1,26 +1,30 @@
 <template>
-   <div id="day-select">
-       <ul class="days">
-           <li class="day" v-for="day in days">{{formatDay(day)}}</li>
-       </ul>
-   </div>
+    <div id="day-select">
+        <ul class="days">
+            <li v-bind:class="{day : true,active:isActive(day)}" v-for="day in days" v-on:click="selected = day">{{formatDay(day)}}</li>
+        </ul>
+    </div>
 </template>
 <script>
-  export default{
-      data(){
-          return{
-            days:[0,1,2,3,4,5,6].map(num => this.$moment().add(num,'days'))
-          };
-      },
-      methods:{
-          formatDay:function(raw){
-              if(raw.isSame(this.$moment(),'day')){
-                  return 'Today'
-              }
-              return raw.format('ddd DD/MM')
-          }
-      }
-  }
+    export default {
+        data() {
+            return {
+                selected: this.$moment(),
+                days: [0, 1, 2, 3, 4, 5, 6].map(num => this.$moment().add(num, 'days'))
+            };
+        },
+        methods: {
+            formatDay: function (raw) {
+                if (raw.isSame(this.$moment(), 'day')) {
+                    return 'Today'
+                }
+                return raw.format('ddd DD/MM')
+            },
+            isActive(day){
+               return day.isSame(this.selected,'day');
+            }
+        }
+    }
 </script>
 <style>
 
